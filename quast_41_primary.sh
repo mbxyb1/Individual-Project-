400ef31550bb8e3b19b73698a1700776aea8ac38
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:28:45 +0100

    Create quast_41_primary.sh

---
+++ quast_41_primary.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=quast_41_primary
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64g
+#SBATCH --time=60:00:00
+#SBATCH --output=/gpfs01/share/Yant_Group_Shared/Yash/OandE/41/quast/primary/%x.out
+#SBATCH --error=/gpfs01/share/Yant_Group_Shared/Yash/OandE/41/quast/primary/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate quast
+
+quast /gpfs01/share/Yant_Group_Shared/Yash/OandE/41/hifiasm/primary/maternal.p_ctg.fa \
+        /gpfs01/share/Yant_Group_Shared/Yash/OandE/41/hifiasm/primary/paternal.p_ctg.fa \
+        /gpfs01/share/Yant_Group_Shared/Yash/OandE/41/hifiasm/primary/maternal.p_utg.fa \
+        /gpfs01/share/Yant_Group_Shared/Yash/OandE/41/hifiasm/primary/paternal.p_utg.fa \
+        -r /share/Yant_Group_Shared/000.refs/Arabidopsis_arenosa/Arabidopsis_arenosa_genome.softmasked.fna.gz \
+        -g /share/Yant_Group_Shared/000.refs/Arabidopsis_arenosa/Arabidopsis_arenosa_genome.annotation.gtf
+
