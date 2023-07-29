8820759694795a262e7ff8a5861309500ad1a66c
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:10:09 +0100

    Create hifiasm_phased_assembly_maternal_21.sh
    hifiasm phased assmebly on maternal pool

---
+++ hifiasm_phased_assembly_maternal_21.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hifiasm_phased_assembly_maternal_21
+#SBATCH --partition=mmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=500g
+#SBATCH --time=168:00:00
+#SBATCH --output=/gpfs01/share/Yant_Group_Shared/Yash/OandE/21/hifiasm/phased/%x.out
+#SBATCH --error=/gpfs01/share/Yant_Group_Shared/Yash/OandE/21/hifiasm/phased/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+/gpfs01/home/mbxyb1/envs/hifiasm/hifiasm -o /gpfs01/share/Yant_Group_Shared/Yash/OandE/21/hifiasm/phased/maternal -t 24 /gpfs01/share/Yant_Group_Shared/Yash/OandE/21/classified/maternal.fa$
+
+
