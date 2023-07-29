44d2f616c247e334b3e793e6b421a3ae8d2d7f09
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:32:30 +0100

    Create quast_101_phased.sh
    Quast script for maternal phased assemblies for kmer 101. Paternal assemblies do not run due to a random error, thus, are excluded.

---
+++ quast_101_phased.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=quast_101_phased
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64g
+#SBATCH --time=40:00:00
+#SBATCH --output=/gpfs01/share/Yant_Group_Shared/Yash/OandE/101/quast/phased/%x.out
+#SBATCH --error=/gpfs01/share/Yant_Group_Shared/Yash/OandE/101/quast/phased/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate quast
+
+quast /gpfs01/share/Yant_Group_Shared/Yash/OandE/101/hifiasm/phased/maternal.bp.hap1.p_ctg.fa \
+        /gpfs01/share/Yant_Group_Shared/Yash/OandE/101/hifiasm/phased/maternal.bp.hap2.p_ctg.fa \
+        -r /share/Yant_Group_Shared/000.refs/Arabidopsis_arenosa/Arabidopsis_arenosa_genome.softmasked.fna.gz \
+        -g /share/Yant_Group_Shared/000.refs/Arabidopsis_arenosa/Arabidopsis_arenosa_genome.annotation.gtf
+
