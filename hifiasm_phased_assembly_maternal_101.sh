989fb6401400b7d0fe3459bd4d595a1c5ccbc0e6
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:24:16 +0100

    Create hifiasm_phased_assembly_maternal_101.sh

---
+++ hifiasm_phased_assembly_maternal_101.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hifiasm_phased_assembly_maternal_101
+#SBATCH --partition=mmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=500g
+#SBATCH --time=168:00:00
+#SBATCH --output=/gpfs01/home/mbxyb1/OandE/101/hifiasm/phased/%x.out
+#SBATCH --error=/gpfs01/home/mbxyb1/OandE/101/hifiasm/phased/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+/gpfs01/home/mbxyb1/envs/hifiasm/hifiasm -o /gpfs01/home/mbxyb1/OandE/101/hifiasm/phased/maternal -t 24 /gpfs01/home/mbxyb1/OandE/101/classified/maternal.fastq.gz
+
+
