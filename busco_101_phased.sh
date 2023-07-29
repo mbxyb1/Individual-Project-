944523ff47082e68b6f9c8d7193d7d1a63c37f63
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:39:41 +0100

    Create busco_101_phased.sh
    Busco script for maternal phased assemblies for kmer 101.
    Paternal assemblies excluded as they do not run due to a random error.

---
+++ busco_101_phased.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=busco_101_phased
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64g
+#SBATCH --time=12:00:00
+#SBATCH --output=/gpfs01/share/Yant_Group_Shared/Yash/OandE/101/busco/phased/%x.out
+#SBATCH --error=/gpfs01/share/Yant_Group_Shared/Yash/OandE/101/busco/phased/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate busco_env
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/101/hifiasm/phased/maternal.bp.hap1.p_ctg.fa -o /results_maternal_hap1 -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/101/hifiasm/phased/maternal.bp.hap2.p_ctg.fa -o /results_maternal_hap2 -l brassicales --cpu 12
+
