c0b6f1a60b8f475c5aa67aa056ee58c034c029ae
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:37:43 +0100

    Create busco_81_phased.sh

---
+++ busco_81_phased.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=busco_81_phased
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64g
+#SBATCH --time=12:00:00
+#SBATCH --output=/gpfs01/share/Yant_Group_Shared/Yash/OandE/81/busco/phased/%x.out
+#SBATCH --error=/gpfs01/share/Yant_Group_Shared/Yash/OandE/81/busco/phased/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate busco_env
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/81/hifiasm/phased/maternal.bp.hap1.p_ctg.fa -o /results_maternal_hap1 -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/81/hifiasm/phased/maternal.bp.hap2.p_ctg.fa -o /results_maternal_hap2 -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/81/hifiasm/phased/paternal.bp.hap1.p_ctg.fa -o /results_paternal_hap1 -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/share/Yant_Group_Shared/Yash/OandE/81/hifiasm/phased/paternal.bp.hap2.p_ctg.fa -o /results_paternal_hap2 -l brassicales --cpu 12
+
+
