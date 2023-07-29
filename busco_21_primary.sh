c34f4b1b79eb03de2cfc902a57cd791303c5ece6
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:35:12 +0100

    Create busco_21_primary.sh
    Busco Script for all primary assemblies for kmer 21.

---
+++ busco_21_primary.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name=busco_21_primary
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64g
+#SBATCH --time=12:00:00
+#SBATCH --output=/gpfs01/home/mbxyb1/OandE/21/busco/primary/%x.out
+#SBATCH --error=/gpfs01/home/mbxyb1/OandE/21/busco/primary/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate busco_env
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/home/mbxyb1/OandE/21/hifiasm/primary/maternal.p_ctg.fa -o /results_maternal_ctg -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/home/mbxyb1/OandE/21/hifiasm/primary/maternal.p_utg.fa -o /results_maternal_utg -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/home/mbxyb1/OandE/21/hifiasm/primary/paternal.p_ctg.fa -o /results_paternal_ctg -l brassicales --cpu 12
+
+~/conda/envs/busco_env/bin/busco -m genome -i /gpfs01/home/mbxyb1/OandE/21/hifiasm/primary/paternal.p_utg.fa -o /results_paternal_utg -l brassicales --cpu 12
+
+
+
+
+
