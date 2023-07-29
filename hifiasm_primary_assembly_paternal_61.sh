b045df917aa6ccaae2f3a1fc0e94efd31047d398
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:20:03 +0100

    Create hifiasm_primary_assembly_paternal_61.sh

---
+++ hifiasm_primary_assembly_paternal_61.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=hifiasm_primary_assembly_paternal_61
+#SBATCH --partition=mmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=500g
+#SBATCH --time=168:00:00
+#SBATCH --output=/gpfs01/home/mbxyb1/OandE/61/hifiasm/primary/%x.out
+#SBATCH --error=/gpfs01/home/mbxyb1/OandE/61/hifiasm/primary/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+/gpfs01/home/mbxyb1/envs/hifiasm/hifiasm -o /gpfs01/home/mbxyb1/OandE/61/hifiasm/primary/paternal --primary -t 24 /gpfs01/home/mbxyb1/OandE/61/classified/paternal.fastq.gz
+
+
+
