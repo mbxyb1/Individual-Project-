7b7c835ec0bf48d532e012c838dcda89985406e1
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 15:03:48 +0100

    Create classify_long_reads_41.sh
    Kmer 41

---
+++ classify_long_reads_41.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=classify_long_reads_41
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32g
+#SBATCH --time=48:00:00
+#SBATCH --output=/gpfs01/home/mbxyb1/OandE/41/%x.out
+#SBATCH --error=/gpfs01/home/mbxyb1/OandE/41/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate trio_binning
+
+classify-by-kmers \
+    /gpfs01/share/Yant_Group_Shared/NEOF_hifi_triobinning/min500bp/F1_500bp.fastq.gz \
+    hapA_only_kmers.txt \
+    hapB_only_kmers.txt \
+    --haplotype-a-out-prefix /gpfs01/home/mbxyb1/OandE/41/classified/maternal \
+    --haplotype-b-out-prefix /gpfs01/home/mbxyb1/OandE/41/classified/paternal \
+    --unclassified-out-prefix /gpfs01/home/mbxyb1/OandE/41/classified/unclassified
