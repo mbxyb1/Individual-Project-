0935045dee938f1f7a4d8c5c191edabb6b2d0950
mbxyb1 <131272560+mbxyb1@users.noreply.github.com>
2023-07-29 14:58:33 +0100

    Create find_unique_kmers_41.sh
    Kmer 41

---
+++ find_unique_kmers_41.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=find_unique_kmers_41
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=32g
+#SBATCH --time=48:00:00
+#SBATCH --output=/gpfs01/home/mbxyb1/OandE/41/%x.out
+#SBATCH --error=/gpfs01/home/mbxyb1/OandE/41/%x.err
+
+source $HOME/.bash_profile
+module load anaconda-uon/3
+
+conda activate trio_binning
+
+fqdir="/gpfs01/share/Yant_Group_Shared/NEOF_hifi_triobinning/min500bp"
+
+find-unique-kmers -k 41 -p 8 ${fqdir}/P1_3168_500bp.fastq.gz ${fqdir}/P2_3177_500bp.fastq.gz
